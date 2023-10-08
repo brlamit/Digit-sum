7506a5000ef2b66b69f69e3d5f9923822e51bd2a
Amit Baral <itsmeamit991@gmai.com>
2023-10-08 21:53:19 +0545

    Add project files.

---
+++ Digit sum/Digit sum/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim n As Integer, r As Integer, sum As Integer = 0
+        n = Console.ReadLine
+        While n > 0
+            r = n Mod 10
+            sum = sum + r
+            n = n / 10
+        End While
+        Console.WriteLine("Sum of digit = " & sum)
+        Console.ReadKey()
+    End Sub
+End Module
